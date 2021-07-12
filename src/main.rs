aa6b4040cde140d251b225eaf737edcd09982989
Zagitta <zetlon+git@gmail.com>
2021-07-12 20:45:41 +0200

    MVP

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use std::{error::Error, sync::Arc, time::Duration};
 
-use clap::{crate_version, App, Arg};
-use mqtt::{
-    packet::{DecodablePacket, EncodablePacket, SubscribePacket, VariablePacket},
-    QualityOfService, TopicFilter,
-};
+use clap::{crate_version, AppSettings, Clap};
+use mqtt_async_client::client::{Client, QoS, Subscribe, SubscribeTopic};
 use serde::{Deserialize, Serialize};
-use tracing::Level;
+use tracing::{info, Level};
 use tracing_subscriber::EnvFilter;
 
-use pid::{ControlOutput, Pid};
+use pid::Pid;
 use tokio::{
-    sync::{self, broadcast, mpsc, RwLock},
+    sync::{broadcast, mpsc, RwLock},
     time,
 };
 
@@ -28,47 +25,45 @@ impl Default for OperatingMode {
     }
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Default, Serialize, Deserialize, Clone, Copy)]
-struct PIDSettings {
-    setpoint: f32,
-    kp: f32,
-    ki: f32,
-    kd: f32,
-    mode: OperatingMode,
-}
-
-impl Into<Pid<f32>> for PIDSettings {
-    fn into(self) -> Pid<f32> {
-        Pid::new(
-            self.kp,
-            self.ki,
-            self.kd,
-            f32::MAX,
-            f32::MAX,
-            f32::MAX,
-            f32::MAX,
-            self.setpoint,
-        )
-    }
-}
 #[derive(Debug, PartialEq, PartialOrd, Serialize, Deserialize)]
 struct ControllerState {
-    settings: PIDSettings,
     pid: Pid<f32>,
     input: f32,
     output: f32,
 }
 
 impl ControllerState {
-    fn new() -> ControllerState {
-        let settings = PIDSettings::default();
+    pub fn new() -> ControllerState {
         ControllerState {
-            pid: settings.into(),
-            settings,
+            pid: Pid::new(1.0, 0.0, 0.0, 100.0, 30.0, 30.0, 100.0, 20.0),
             input: 0.0,
             output: 0.0,
         }
     }
+}
+
+#[derive(Clap)]
+#[clap(name = "rusty-fridge")]
+#[clap(version = crate_version!(), author = "Simon Rasmussen <zetlon@gmail.com>")]
+#[clap(
+    about = "Controls an MQTT on/off switch to regulate the temperature of a fridge for beer fermenting"
+)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct CLIArgs {
+    #[clap(short, long)]
+    server: String,
+    #[clap(short, long)]
+    username: String,
+    #[clap(short, long)]
+    password: String,
+    #[clap(short, long, default_value = "rusty-fridge-{}")]
+    client_id: String,
+    #[clap(short, long, default_value = "rusty-fridge")]
+    topic_prefix: String,
+    #[clap(short, long)]
+    output_topic: String,
+    #[clap(short, long)]
+    input_topic: String,
 }
 
 #[tokio::main]
@@ -76,70 +71,86 @@ async fn main() -> std::result::Result<(), Box<dyn Error>> {
     tracing::subscriber::set_global_default(
         tracing_subscriber::fmt()
             .with_writer(std::io::stderr)
-            .with_max_level(Level::INFO)
+            .with_max_level(Level::TRACE)
             .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_default())
             .finish(),
     )?;
 
-    let matches = App::new("rusty-fridge")
-        .version(crate_version!())
-        .author("Simon Rasmussen <zetlon@gmail.com>")
-        .about("Controls an MQTT on/off switch to regulate the temperature of a fridge for beer fermenting")
-        .arg(Arg::with_name("server").env("RF_SERVER").required(true).takes_value(true).help("MQTT server address (host:port)"))
-        .arg(Arg::with_name("username").env("RF_USERNAME").takes_value(true).required(true))
-        .arg(Arg::with_name("password").env("RF_PASSWORD").takes_value(true).required(true).help("Password provided as raw password or a path from which to read the pw from"))
-        .arg(Arg::with_name("client_id").env("RF_CLIENT_ID").takes_value(true).default_value("rusty-fridge"))
-        .arg(Arg::with_name("prefix").takes_value(true).default_value("rusty-fridge"))
-        .get_matches();
-
-    let pw = {
-        let pw = matches.value_of("password").unwrap();
-        if let Ok(f) = std::fs::read_to_string(pw) {
-            f
-        } else {
-            pw.to_owned()
+    let args = {
+        let mut a = CLIArgs::parse();
+        if let Ok(f) = std::fs::read_to_string(&a.password) {
+            a.password = f;
         }
+        a
     };
 
-    let prefix = "foobar";
-    SubscribePacket::new(
-        0,
-        vec![(
-            TopicFilter::new(format!("{}/control", prefix)).unwrap(),
-            QualityOfService::Level2,
-        )],
-    );
+    let mut split = args.server.split(':');
+    let host = split.next().unwrap().to_owned();
+    let port = split.next().unwrap().parse()?;
+
+    let mut client = Client::builder()
+        .set_host(host)
+        .set_port(port)
+        .set_username(Some(args.username))
+        .set_password(Some(args.password.as_bytes().to_vec()))
+        .build()?;
+
+    client.connect().await?;
+    info!("connected!");
+
+    let client = RwLock::new(client);
+
+    let topic = args.input_topic.clone();
+    let t1 = tokio::spawn(async {
+        info!(?topic, "subscribing to topic");
+        let client = client;
+        let mut lock = client.write().await;
+        let subs = lock
+            .subscribe(Subscribe::new(vec![SubscribeTopic {
+                topic_path: topic,
+                qos: QoS::AtLeastOnce,
+            }]))
+            .await;
+
+        info!(?subs, "Subscribed!");
+        loop {
+            let foobar = lock.read_subscriptions().await.unwrap();
+            info!(?foobar, "got msg");
+        }
+    });
+
+    let res = tokio::join!(t1);
 
     Ok(())
 }
 
-async fn setting_task(
-    state: RwLock<ControllerState>,
-    sub_queue: mpsc::Sender<SubscribePacket>,
-    mut msg_queue: broadcast::Receiver<Arc<VariablePacket>>,
-) {
-    let cf = TopicFilter::new(format!("{}/control", "asd")).unwrap();
-    sub_queue
-        .send(SubscribePacket::new(
-            0,
-            vec![(cf, QualityOfService::Level2)],
-        ))
-        .await
-        .expect("Should always be able to subscribe");
-    loop {
-        if let Ok(arc) = msg_queue.recv().await {
-            match *arc {
-                VariablePacket::PublishPacket(ref pkg) => {
-                    /* if cf.matches(pkg.topic_name()) {
-                        let payload = pkg.payload();
-                        //json?
-                    } */
-                }
-                _ => {}
-            }
-        }
-    }
-}
+// async fn setting_task(
+//     state: RwLock<ControllerState>,
+//     sub_queue: mpsc::Sender<SubscribePacket>,
+//     mut msg_queue: broadcast::Receiver<Arc<VariablePacket>>,
+// ) {
+//     let cf = TopicFilter::new(format!("{}/control", "asd")).unwrap();
+//     sub_queue
+//         .send(SubscribePacket::new(
+//             0,
+//             vec![(cf, QualityOfService::Level2)],
+//         ))
+//         .await
+//         .expect("Should always be able to subscribe");
+//     loop {
+//         if let Ok(arc) = msg_queue.recv().await {
+//             match *arc {
+//                 VariablePacket::PublishPacket(ref pkg) => {
+//                     /* if cf.matches(pkg.topic_name()) {
+//                         let payload = pkg.payload();
+//                         //json?
+//                     } */
+//                 }
+//                 _ => {}
+//             }
+//         }
+//     }
+// }
 
 async fn pid_task() {
     let mut state = ControllerState::new();
